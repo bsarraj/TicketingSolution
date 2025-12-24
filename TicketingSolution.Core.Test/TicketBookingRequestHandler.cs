d641c9dc9eb61db61a753c2e57f16063a74fdcb0
Behnam Sarraj <behnam.sarraj@gmail.com>
2025-12-24 13:54:46 +0330

    Add project files.

---
+++ TicketingSolution.Core.Test/TicketBookingRequestHandler.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+namespace TicketingSolution.Core.Test
+{
+    internal class TicketBookingRequestHandler
+    {
+        public TicketBookingRequestHandler()
+        {
+        }
+
+        internal ServiceBookingResult BookService(TicketBookingRequest bookingRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TicketingSolution.Core.Test/TicketBookingRequestHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TicketingSolution.Core.Test
+{
+    public class Ticket_Booking_Request_Handler_Test
+    {
+        [Fact]
+        public void Shuld_Return_Ticket_Booking_Response_With_Request_Values()
+        {
+
+            var BookingRequest = new TicketBookingRequest
+            {
+                Name = "Test",
+                Family = "Family",
+                Email = "Email"
+            };
+
+            var Handler = new TicketBookingRequestHandler();
+
+            ServiceBookingResult result = Handler.BookService(BookingRequest);
+
+            
+            Assert.NotNull(result);
+
+        }
+    }
+}
