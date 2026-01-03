e085747bc5e25fd0ed6f07c9a9b6866ecabd41fb
Behnam Sarraj <behnam.sarraj@gmail.com>
2026-01-03 17:55:29 +0330

    Add API

---
+++ TicketingSolution.API.Test/TicketSolutionApiServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.Common.Exceptions;
+using TicketingSolution.API.Controllers;
+using Moq;
+using Shouldly;
+
+
+namespace TicketingSolution.API.Test
+{
+    public class TicketSolutionApiServiceTest
+    {
+        [Fact]
+        public void Should_Return_Forcast_Results()
+        {
+            //Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            result.Count().ShouldBeGreaterThan(1);
+            result.ShouldNotBeNull();
+
+        }
+    }
+}
