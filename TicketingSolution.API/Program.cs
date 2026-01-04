1be20831692d4fe2c90dc018b5e6c724ebafb04a
Behnam Sarraj <behnam.sarraj@gmail.com>
2026-01-04 15:12:56 +0330

    Set up in-memory SQLite with EF Core for API project
    Added Microsoft.Data.Sqlite and EF Core (with SQLite provider) dependencies. Configured Program.cs to use an in-memory SQLite database and registered TicketingSolutionDbContext with DI. Added project reference to TicketingSolution.Persistence for persistence logic.

--- TicketingSolution.API/Program.cs
+++ TicketingSolution.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.Data.Sqlite;
+using TicketingSolution.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connString = "DataSource=:memory:";
+var conn = new SqliteConnection(connString);
+conn.Open();
+
+builder.Services.AddDbContext<TicketingSolutionDbContext>(opt => opt.UseSqlite(conn));
 
 var app = builder.Build();
 
